d73c9a06e9ce94741152f59b1770b8504b387cd5
Matt Greenfield <gmattgreenfield@gmail.com>
2016-05-14 11:18:32 +0100

    output links and change on click

--- app.js
+++ app.js
@@ -1,63 +1,91 @@
 
 var peopleData = new Object();
+var linksData = new Object();
+var currentlyDisplayedPeople = {};
 
-$.get("data/people.json", function (result) {
+$.get("data/relationships.json", function (result) {
 
-    // console.log(result);
-    peopleData = result[0];
+    linksData = result;
+    // console.log(linksData);
 
-    console.log(peopleData.john);
-
-    runReact();
+    // now we have the relationships data, get the people data
+    getPeople()
 });
 
 
-// console.log(peopleData.john.firstname);
+function getPeople(){
 
-function runReact(){
+    $.get("data/people.json", function (result) {
 
-    var OutputPerson = React.createClass({
+        peopleData = result[0];
+        // console.log(peopleData.john);
 
-        handleClick: function() {
-            console.log("clicked");
-        },
-
-        render: function() {
-            var person = String(this.props.name);
-
-            // console.log(person);
-            // console.log(peopleData);
-            // console.log(peopleData[person]);
-
-            return (
-                <div className="artist" onClick={this.handleClick}>
-
-                    <p>{peopleData[person].name.first} {peopleData[person].name.last}</p>
-                    <img src={peopleData[person].picture} />
-                    <details>
-                        <summary>Read Bio</summary>
-                        {peopleData[person].about}
-                    </details>
-                    <ul>
-                        <li></li>
-                    </ul>
-                </div>
-            );
-        }
+        // Now we have the people data, run react and render the site
+        ReactDOM.render(
+            < OutputPerson name="paul" />,
+            document.getElementById('example')
+        );
     });
-
-    var OutputLinks = React.createClass({
-        render: function() {
-            return <div>
-            < OutputPerson name="john" />
-            < OutputPerson name="paul" />
-            < OutputPerson name="ringo" />
-            </div>
-        }
-    })
-
-    ReactDOM.render(
-        < OutputPerson name="paul" />,
-        document.getElementById('example')
-    );
 }
+
+
+// Component: person
+var OutputPerson = React.createClass({
+
+    render: function() {
+        // the name passed to this function
+        var person = String(this.props.name);
+        // console.log(person);
+
+        return (
+            <div className="artist">
+                <p>{peopleData[person].name.first} {peopleData[person].name.last}</p>
+                <img src={peopleData[person].picture} />
+                <details>
+                    <summary>Read Bio</summary>
+                    {peopleData[person].about}
+                </details>
+                < OutputLinks {...this.props} />
+            </div>
+        );
+    }
+});
+
+
+// Component: links
+var OutputLinks = React.createClass({
+
+    handleClick: function(name) {
+
+        console.log("clicked:"+name);
+
+        // var parent = findAncestor( document.getElementById('js-link-to-{name}'), 'artist');
+        ReactDOM.render(
+            < OutputPerson name={name} />,
+            document.getElementById('example')
+        );
+    },
+
+    render: function() {
+
+        // Get the person we want to find links for
+        var person = String(this.props.name);
+        var links = [];
+        // console.log(linksData.length);
+
+        for(var i = 0; i < linksData.length; i++){
+
+            var peopleInLink = linksData[i].people;
+            // console.log(peopleInLink);
+
+            for (var i = 0; i < peopleInLink.length; i++) {
+                var name = peopleInLink[i];
+                // the onClick may look a bit odd, really its just `onClick="handleClick(name)"`. See http://stackoverflow.com/a/20446806/3098555
+                links.push(<li key={i} id="js-link-to-{name}" onClick={this.handleClick.bind(null, name)}>{name}</li>);
+            }
+
+        }
+
+        return <div> Links: <ul className="list--inline">{links}</ul></div>
+    }
+})
