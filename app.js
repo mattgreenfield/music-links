1a1b29892d1a4b28c29ceef1a5544b2558467666
Matt Greenfield <gmattgreenfield@gmail.com>
2016-05-24 23:39:24 +0100

    add connection counter

--- app.js
+++ app.js
@@ -32,6 +32,31 @@ function buildPage(){
     );
 }
 
+
+
+// Function: get person data
+var personPicture, personFullName, personBio;
+function GetPersonData(person){
+    personPicture = peopleData[person].picture;
+    personFullName = peopleData[person].name.first + " " + peopleData[person].name.last;
+    personBio = peopleData[person].about;
+};
+
+
+// Function to render the given 'name' as a new card in the page
+function renderNewPerson(name, linkedFrom) {
+
+    // console.log("Clicked: "+name);
+
+    // add the clicked person to the peopleToShowArray array,
+    var object = {person: name, link: linkedFrom};
+    peopleToShowArray.push(object);
+
+    // Rebuild the page with the new peopleToShowArray values
+    buildPage();
+};
+
+
 // Component: page
 var peopleToShowArray = [
     {person: 'john', link: 'john'}
@@ -56,6 +81,7 @@ var OutputPage = React.createClass({
             <main>
                 {peopleList}
                 < OutputLinkOptions name={lastPerson}/>
+                <div className="connections-counter"><span>{peopleToShowArray.length - 1}</span> Connections</div>
             </main>
         );
     }
@@ -64,54 +90,27 @@ var OutputPage = React.createClass({
 
 
 
-
-// Component: person
+// Component: output a person card
 var OutputPerson = React.createClass({
-
-    // Function to render the given 'name' as a new card in the page
-    renderNewPerson: function(name, linkedFrom) {
-
-        // console.log("Clicked: "+name);
-
-        // if they aren't already in the peopleToShowArray array, add them to it
-        // if( peopleToShowArray.indexOf(name) == -1 ){
-            var object = {person: name, link: linkedFrom};
-            peopleToShowArray.push(object);
-        // }
-        // else {
-        //     alert("They're already on the page.");
-        // }
-        // console.log(peopleToShowArray);
-
-        // Now we have the people data, run react and render the site
-        buildPage();
-    },
 
     render: function() {
         // the name passed to this function
         var person = String(this.props.name);
         var linkedFrom = this.props.linkedFrom;
 
+        // Get the data for this person
+        GetPersonData(person);
 
         // console.log(person);
 
-        var clickEvent;
-        // if this is a link, add the renderNewPerson() function onclick
-        if(this.props.link){
-            // Note: the onClick may look a bit odd, really its just `onClick="renderNewPerson(name)"`. See http://stackoverflow.com/a/20446806/3098555
-            clickEvent = this.renderNewPerson.bind(null, person, linkedFrom);
-        }
-        else {
-            // TODO: Reset the chain up to this person, maybe with a "are you sure"
-        }
-
         return (
-                <article className="artist" onClick={clickEvent}>
+                <article className="artist">
                     <div className="artist__image">
-                        <img src={peopleData[person].picture} />
+                        <img src={personPicture} />
                     </div>
                     <div className="artist__text">
-                        <h1>{peopleData[person].name.first} {peopleData[person].name.last}</h1>
+                        <h1>{personFullName}</h1>
+                        <p>{personBio}</p>
                     </div>
                 </article>
         );
@@ -148,10 +147,13 @@ var OutputLink = React.createClass({
 // Component: link options
 var OutputLinkOptions = React.createClass({
 
+    clickEvent: function(linkingTo, linkedFrom) {
+        renderNewPerson(linkingTo, linkedFrom)
+    },
     render: function() {
 
         // Get the person we want to find links for
-        var person = this.props.name;
+        var linkedFrom = this.props.name;
         var links = [];
         // console.log(linksData.length);
 
@@ -162,23 +164,28 @@ var OutputLinkOptions = React.createClass({
             // console.log(peopleInLink);
 
             // Check if the person whos card we are building is in this entry of the json
-            if( peopleInLink.indexOf(person) != -1 ){
+            if( peopleInLink.indexOf(linkedFrom) != -1 ){
                 // console.log("we have a link");
 
                 // Loop through all the names in that link / json entry
                 for (var a = 0; a < peopleInLink.length; a++) {
-                    var linkName = peopleInLink[a];
+                    // the person we're linking too
+                    var linkingTo = peopleInLink[a];
 
-                    // Don't output the person as a link to themselves
-                    // @TODO: stop it outputting if the name is already in the `links` array / on the page?
-                    if(linkName != person){
-                        links.push(<OutputPerson key={a} name={linkName} linkedFrom={person} link="true"/>);
+                    // Don't output the person as a link to themselves OR if they are already on the page
+                    if( linkingTo != linkedFrom ){
+                        // Get the data for this person
+                        GetPersonData(linkingTo);
+                        // Note: the onClick may look a bit odd, really its just `onClick="renderNewPerson(name)"`. See http://stackoverflow.com/a/20446806/3098555
+                        var clickEvent = this.clickEvent.bind(null, linkingTo, linkedFrom);
+                        // Add the markup
+                        links.push(<li key={a} onClick={clickEvent} ><p>{personFullName}</p><img src={personPicture}/></li>);
                     }
                 }
             }
 
         }
 
-        return <section className="row row--links"><h1>Choose A Link</h1>{links}</section>
+        return <section className="row row--links"><h1>Choose A Link</h1><ul className="link-choices">{links}</ul></section>
     }
 })
