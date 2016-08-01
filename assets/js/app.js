43659ee43cae328070267670bf22ef92178fa6ab
Matt Greenfield <gmattgreenfield@gmail.com>
2016-08-01 21:16:17 +0100

    strip things back

--- assets/js/app.js
+++ assets/js/app.js
@@ -114,6 +114,8 @@ var linksData = [{
 function wikiData(searchTerm, callBack) {
 
     // replace spaces with %20 etc etc.
+    // var searchTerm = searchTerm.replace(" ","").toLowerCase();
+    // console.log(searchTerm);
     var searchTerm = encodeURIComponent(camelToSpace(searchTerm));
 
     var article = {
@@ -124,31 +126,18 @@ function wikiData(searchTerm, callBack) {
 
     $.ajax({
         // Search wikipedia for our term
-        // url:'https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch='+ searchTerm +'%22AND%22Born%22AND%22Occupation%22&format=jsonfm&srprop=snippet&srlimit=1',
-        url: 'https://en.wikipedia.org/w/api.php?action=query&list=embeddedin&titles=' + searchTerm + '&prop=extracts&eititle=Template%E2%80%8C%E2%80%8B:Persondata&format=json',
-        // url: 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch='+ searchTerm,
+        url: 'https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=' + searchTerm + '%22AND%22Born%22AND%22music&format=jsonfm&srlimit=1',
         data: {
             format: 'json'
         },
         dataType: 'jsonp',
         success: function success(data) {
-            console.log(data);
 
-            // Get first object within object (the first search result). See http://stackoverflow.com/a/909058/3098555
-            var obj = data.query.pages;
-            var article;
-            for (var i in obj) {
-                if (obj.hasOwnProperty(i) && typeof i !== 'function') {
-                    article = obj[i];
-                    break;
-                }
-            }
-            console.log(article);
+            var article = {};
 
             // Set the name and description as the result from wikipedia
-            article.name = article.title;
-            article.description = article.extract;
-            // article.url = data[3][0];
+            article.name = data.query.search[0].title;
+            article.description = data.query.search[0].snippet.replace(/<(?:.|\n)*?>/gm, '');
             // @TODO Load the image
 
             if (typeof callBack === 'function') {
